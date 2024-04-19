7d77cc69bd9199e8b319bde4b86da78454b7ab89
Nicolay <nicolaymorenoherrera@gmail.com>
2024-04-19 11:18:59 -0500

    Adding notes

--- src/solid/01-srp.ts
+++ src/solid/01-srp.ts
@@ -1,36 +1,36 @@
 (() => {
 
+    //! Not SRP.
+
     interface Product {
         id:   number;
         name: string;
     }
 
-    // Usualmente, esto es una clase para controlar la vista que es desplegada al usuario
-    // Recuerden que podemos tener muchas vistas que realicen este mismo trabajo.
+    // Usually, this is a class to control the view displayed to the user
+    // Remember that we can have many views that perform this same task.
     class ProductBloc {
 
         loadProduct( id: number ) {
-            // Realiza un proceso para obtener el producto y retornarlo
-            console.log('Producto: ',{ id, name: 'OLED Tv' });
+            // Performs a process to fetch the product and return it
+            console.log('Product:', { id, name: 'OLED Tv' });
         }
 
         saveProduct( product: Product ) {
-            // Realiza una petición para salvar en base de datos
-            console.log('Guardando en base de datos', product );
+            // Makes a request to save in the database
+            console.log('Saving to the database', product );
         }
 
         notifyClients() {
-            console.log('Enviando correo a los clientes');
+            console.log('Sending emails to clients');
         }
 
         onAddToCart( productId: number ) {
-            // Agregar al carrito de compras
-            console.log('Agregando al carrito ', productId );
+            // Add to cart
+            console.log('Adding to cart', productId );
         }
 
     }
-
-
 
     const productBloc = new ProductBloc();
 
@@ -39,11 +39,21 @@
     productBloc.notifyClients();
     productBloc.onAddToCart(10);
 
-
-
-
-
-
-
-
 })();
+
+/*
+
+    !Not SRP.
+
+    ? The original code violates the SRP (Single Responsibility Principle) because the `ProductBloc` class assumes multiple responsibilities, including loading and saving products, sending email notifications to clients, and managing the shopping cart. The proposed solution divides these responsibilities into separate classes:
+
+    * - `ProductService`: Responsible for operations related to products, such as fetching and saving product data.
+    * - `Mailer`: Handles sending emails, with options for different email templates.
+    * - `ProductBloc`: Orchestrates the interaction between `ProductService` and `Mailer`, ensuring adherence to the Single Responsibility Principle. It loads and saves products via `ProductService` and notifies clients via `Mailer`.
+    * - `CartBloc`: Manages operations related to shopping cart functionality, specifically adding items to the cart.
+
+    * This solution improves code clarity and maintainability by separating responsibilities into distinct classes, each with a clear and focused purpose.
+
+    ? See example "01-srp.solution.ts".
+
+*/
--- src/solid/02-open-close-ocp-b.ts
+++ src/solid/02-open-close-ocp-b.ts
@@ -1,11 +1,15 @@
 // We need to add the axios dependency ```yarn add axios```
 
+
+//! Not OCP.
+
 import axios from 'axios';
 
 
 export class TodoService {
 
     async getTodoItems() {
+
         const { data } = await axios.get('https://jsonplaceholder.typicode.com/todos/');
         return data;
     }
@@ -28,3 +32,15 @@ export class PhotosService {
         return data;
     }
 }
+
+/*
+
+    ! Not OCP.
+
+    * The code violates the OCP principle because each service is tightly coupled to a specific API URL. If a new service is needed to retrieve data from another source, each existing class would have to be modified. To comply with the OCP principle, the data source logic should be decoupled from the services, allowing extension without modifying the existing code.
+
+    * For example, in the solution, we could decouple the data source logic by creating a separate class to handle HTTP requests, and then injecting that dependency into each service that needs to make requests. This enables system extension without modifying existing classes.
+
+    ? See example 02-open-close-ocp-b-solution.ts.
+
+*/
